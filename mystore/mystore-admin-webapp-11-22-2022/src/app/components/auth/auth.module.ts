edafa5837e04c61dd18dd413500a048781ccc33d
hodasanij <hodahds@gmail.com>
2022-11-23 18:05:35 -0800

    added header, navbar and footer

--- mystore/mystore-admin-webapp-11-22-2022/src/app/components/auth/auth.module.ts
+++ mystore/mystore-admin-webapp-11-22-2022/src/app/components/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LoginComponent } from './login/login.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, ForgetPasswordComponent],
+  imports: [CommonModule],
 })
-export class AuthModule { }
+export class AuthModule {}
